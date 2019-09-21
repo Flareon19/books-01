fdfb114a552e35f48cac7d885093e1b16135fe43
Flareon19 <aparna.rani_iot18@gla.ac.in>
2019-09-21 18:11:13 +0530

    * Create required fields as private. * Define public getters/setters for the fields. * Define a constructor. * Override tostring() according to desired values. * Create main method in Execution Class. * Create scanner object. * Create bookClass object and take inputs using Scanner object.

--- src/Solution.java
+++ src/Solution.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        if (n == 0) {
+            System.out.print("N/A");
+        } else {
+            while (n != 0) {
+                Books book = new Books(scanner.nextLine(), scanner.nextLine(), scanner.nextLong());
+                scanner.nextLine();
+                System.out.println("\n-----------------------------\n" + book + "\n-----------------------------\n");
+                n--;
+            }
+        }
     }
 }
